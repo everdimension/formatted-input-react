90364460f084abba937a2784fb6a4dee49980d5c
everdimension <everdimension@gmail.com>
2017-06-26 20:37:40 +0300

    create example component for demo and testing

---
+++ examples/components/Controlled.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import NumberInput from '../../src';
+
+class Controlled extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      exampleValue: '7888',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.setValue = this.setValue.bind(this);
+    this.setExampleValue = this.setExampleValue.bind(this);
+  }
+
+  handleChange(name, value) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  setExampleValue(event) {
+    this.setState({ exampleValue: event.target.value });
+  }
+
+  setValue() {
+    this.setState({ value: this.state.exampleValue });
+  }
+
+  render() {
+    const { value, exampleValue } = this.state;
+    return (
+      <div>
+        <p>Current value: {value}</p>
+        <p>
+          <button onClick={this.setValue}>set value to</button>
+          <input
+            type="text"
+            onChange={this.setExampleValue}
+            value={exampleValue}
+          />
+        </p>
+        <NumberInput
+          name="value"
+          value={value}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default Controlled;
